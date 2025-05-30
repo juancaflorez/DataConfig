8b4c8ae8a2505f465e56790373a55e8fd2f547e4
Juan Florez <juancahf@protonmail.com>
2025-05-30 18:05:05 -0500

    refactor: nested directory

---
+++ Source/DataConfigHeadless.Target.cs
@@ -0,0 +1,51 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+//	UE5.4 onwards this `DataConfigHeadless.Target.cs` must recides here
+//	under `Source` otherwise it won't build.
+
+public class DataConfigHeadlessTarget : TargetRules
+{
+	public DataConfigHeadlessTarget(TargetInfo Target) : base(Target)
+	{
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		Type = TargetType.Program;
+		LaunchModuleName = "DataConfigHeadless";
+		//	! note that only monolithic works atm
+		LinkType = TargetLinkType.Monolithic;
+
+		MinGameModuleSourceFilesForUnityBuild = 2;
+
+		//	! need this for logging
+		bUseLoggingInShipping = true;
+
+		bCompileICU = false;
+
+		bCompileAgainstEngine = false;
+		bCompileAgainstCoreUObject = true;
+		bCompileAgainstApplicationCore = true;
+
+		bForceEnableExceptions = false;
+
+		bCompileWithPluginSupport = false;
+		bBuildDeveloperTools = false;
+
+		bIsBuildingConsoleApplication = true;
+
+#if UE_5_1_OR_LATER
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+#endif
+
+#if UE_5_3_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+#endif
+		//	toggle to use clang-cl
+		//	it works mostly except breaks FName natvis
+		// WindowsPlatform.Compiler = WindowsCompiler.Clang;
+
+		//	toggle to buld with no `WITH_EDITORONLY_DATA`
+		// bBuildWithEditorOnlyData = false;
+		bBuildWithEditorOnlyData = true;
+	}
+}
---
+++ Source/DataConfigHeadless/DataConfigHeadless.Build.cs
@@ -0,0 +1,33 @@
+using UnrealBuildTool;
+using System.IO;
+
+public class DataConfigHeadless : ModuleRules
+{
+	public DataConfigHeadless(ReadOnlyTargetRules Target) : base(Target)
+	{
+		bRequiresImplementModule = false;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"Core",
+				"CoreUObject",
+				"ApplicationCore",
+				"Projects",
+				"DataConfigCore",
+				"DataConfigExtra",
+				"DataConfigTests",
+#if UE_5_0_OR_LATER
+				"DataConfigTests5",
+#endif
+#if UE_5_4_OR_LATER
+				"DataConfigTests54",
+#endif
+			});
+
+		PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+				"Launch",
+			});
+	}
+}
